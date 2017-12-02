58894cdba32ee6d64709ff7846094c2e8d11eb0d
Ratson <github@ratson.name>
2017-12-02 16:32:44 +0800

    Remove quiet option

--- cli.js
+++ cli.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 'use strict'
 
+const _ = require('lodash')
+const split = require('split')
+const debug = require('debug')
+
 const runa = require('.')
 
-function main() {
-  return runa({ quiet: false })
+async function main() {
+  const processes = await runa()
+  const taskNameLen = _.max(Object.keys(processes).map(_.size))
+
+  return _.map(processes, (child, name) => {
+    child.stdout
+      .pipe(split(null, null, { trailing: false }))
+      .on('data', line => {
+        debug(name.padEnd(taskNameLen))(line)
+      })
+  })
 }
 
 main()
--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const _ = require('lodash')
 const debug = require('debug')
 const execa = require('execa')
 const pkgConf = require('pkg-conf')
-const split = require('split')
 
-module.exports = async ({ cwd, quiet = true } = {}) => {
+module.exports = async ({ cwd } = {}) => {
   debug.enable('*')
   // eslint-disable-next-line no-console
   debug.log = console.log.bind(console)
@@ -18,7 +17,6 @@ module.exports = async ({ cwd, quiet = true } = {}) => {
     },
   })
   const execaOpts = { cwd }
-  const taskNameLen = _.max(Object.keys(conf.tasks).map(_.size))
   const processes = _.pickBy(
     _.mapValues(conf.tasks, task =>
       matches(task)(
@@ -28,14 +26,5 @@ module.exports = async ({ cwd, quiet = true } = {}) => {
       )
     )
   )
-  if (quiet) {
-    return processes
-  }
-  return _.map(processes, (child, name) => {
-    child.stdout
-      .pipe(split(null, null, { trailing: false }))
-      .on('data', line => {
-        debug(name.padEnd(taskNameLen))(line)
-      })
-  })
+  return processes
 }
