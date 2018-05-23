e237c95af61cf8e7faa4a6ef69487de1a5c2f669
Ratson <github@ratson.name>
2018-05-23 22:29:19 +0800

    Test package can be required

---
+++ test/require.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+import glob from 'fast-glob'
+import Bluebird from 'bluebird'
+import execa from 'execa'
+
+test('can require packages', async t => {
+  await Bluebird.map(
+    glob('packages/runa-core', { onlyDirectories: true }),
+    async cwd => {
+      await execa('node', ['-e', "require('.')"], { cwd })
+    },
+  )
+  t.pass()
+})
