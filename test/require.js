b8e71bda418054158ac3888b89b31817d314ad98
Ratson <github@ratson.name>
2020-11-26 01:51:48 +0800

    build: lint with xo

--- test/require.js
+++ test/require.js
@@ -3,12 +3,12 @@ import glob from 'fast-glob'
 import Bluebird from 'bluebird'
 import execa from 'execa'
 
-test('can require packages', async t => {
+test('can require packages', async (t) => {
   await Bluebird.map(
-    glob('packages/runa-core', { onlyDirectories: true }),
-    async cwd => {
-      await execa('node', ['-e', "require('.')"], { cwd })
-    },
+    glob('packages/runa-core', {onlyDirectories: true}),
+    async (cwd) => {
+      await execa('node', ['-e', "require('.')"], {cwd})
+    }
   )
   t.pass()
 })
